44cf2d3135a871372dbba56d0f3884d1923f8af8
olsonem <olsonem@seattleu.edu>
2017-08-21 00:52:16 -0400

    tried new js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var card = document.getElementById('mary');
+// DOMContentLoaded listener
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM fully loaded.");
+  })
+
+let card = document.getElementByClass('card');
 
 card.addEventListener('click', function() {
 	if (!this.classList.contains('front')) {
@@ -8,4 +13,14 @@ card.addEventListener('click', function() {
 		this.classList.remove('back');
 		this.classList.add('front');
 	}
+});
+
+let info = document.getElementById('artistInfo')
+
+showInfo.addEventListener('click',function (){
+    if (info.style.display === 'block') {
+        info.style.display = 'none';
+    } else {
+        info.style.display = 'block';
+    }
 });
