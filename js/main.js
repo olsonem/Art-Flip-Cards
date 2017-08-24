bbe913e97399936c786380c88c6016e41048cadc
olsonem <olsonem@seattleu.edu>
2017-08-24 15:21:15 -0400

    JS for touch screens working!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-document.querySelector("#myCard").classList.toggle("flip")
+cards = document.querySelectorAll('.card');
+
+for (let card of cards){
+  card.addEventListener('click', function(event){
+    event.currentTarget.classList.toggle("flip");
+  });
+}
